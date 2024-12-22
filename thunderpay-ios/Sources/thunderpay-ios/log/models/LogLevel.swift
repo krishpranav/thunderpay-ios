accb25ff7ed80b2730701bac687951414b551ff5
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2024-12-22 14:24:18 +0530

    thunderpay-ios: models[LogLevel]

---
+++ thunderpay-ios/Sources/thunderpay-ios/log/models/LogLevel.swift
@@ -0,0 +1,14 @@
+//
+//  LogLevel.swift
+//  thunderpay-ios
+//
+//  Created by Krisna Pranav on 22/12/24.
+//
+
+import Foundation
+
+public struct LogLevel: OptionSet {
+    public init(rawValue: RawValue) {
+        self.rawValue = rawValue
+    }
+}
