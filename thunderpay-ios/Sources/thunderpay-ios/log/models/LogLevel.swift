2130bfd41a0375683555dffec5586aa351ce1f6a
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2024-12-22 14:26:22 +0530

    thunderpay-ios: models[LogLevel]

--- thunderpay-ios/Sources/thunderpay-ios/log/models/LogLevel.swift
+++ thunderpay-ios/Sources/thunderpay-ios/log/models/LogLevel.swift
@@ -11,4 +11,19 @@ public struct LogLevel: OptionSet {
     public init(rawValue: RawValue) {
         self.rawValue = rawValue
     }
+    
+    public static let off = LogLevel(rawValue: offBitmask)
+    public static let debug = LogLevel(rawValue: debugBitmask)
+    public static let info = LogLevel(rawValue: infoBitmask)
+    
+    
+    public typealias RawValue = UInt
+    
+    
+    private static let rawValue: RawValue
+    private static let offBitmask: RawValue = 1 << 0
+    private static let debugBitmask: RawValue = 1 << 1
+    private static let infoBitmask: RawValue = 1 << 2
+    private static let errorBitmask: RawValue = 1 << 3
+    
 }
