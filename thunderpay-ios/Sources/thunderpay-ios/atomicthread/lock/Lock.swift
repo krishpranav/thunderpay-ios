74d1e40828c4c87d3b3d5fba6b6ca21a266bbaa2
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2024-12-22 14:20:12 +0530

    thunderpay-ios: atomicthread[Lock]

--- thunderpay-ios/Sources/thunderpay-ios/atomicthread/lock/Lock.swift
+++ thunderpay-ios/Sources/thunderpay-ios/atomicthread/lock/Lock.swift
@@ -5,3 +5,24 @@
 //  Created by Krisna Pranav on 22/12/24.
 //
 
+import Foundation
+
+protocol Lock {
+    func lock()
+    
+    func unlock()
+}
+
+extension Lock {
+    func around<T>(_ closure: () throws -> T) rethrows -> T {
+        lock()
+        defer { unlock() }
+        return try closure()
+    }
+    
+    func around(_ closure: () throws -> Void) rethrows {
+        lock()
+        defer { unlock() }
+        try closure()
+    }
+}
