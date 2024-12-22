100da86e674ac3d956fea0681f087b18bf9c1787
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2024-12-22 14:05:11 +0530

    thunderpay

---
+++ thunderpay-ios/thunderpay_ios.h
@@ -0,0 +1,18 @@
+//
+//  thunderpay_ios.h
+//  thunderpay-ios
+//
+//  Created by Krisna Pranav on 22/12/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for thunderpay_ios.
+FOUNDATION_EXPORT double thunderpay_iosVersionNumber;
+
+//! Project version string for thunderpay_ios.
+FOUNDATION_EXPORT const unsigned char thunderpay_iosVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <thunderpay_ios/PublicHeader.h>
+
+
---
+++ thunderpay-iosTests/thunderpay_iosTests.swift
@@ -0,0 +1,17 @@
+//
+//  thunderpay_iosTests.swift
+//  thunderpay-iosTests
+//
+//  Created by Krisna Pranav on 22/12/24.
+//
+
+import Testing
+@testable import thunderpay_ios
+
+struct thunderpay_iosTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
